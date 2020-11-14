ab852c7340f0f2ca31c286fb72f3fd5b7edc7bb7
Marius Jørgensen <mariusjorg93@gmail.com>
2020-11-14 20:48:00 +0100

    Force explicit function return type

--- packages/eslint-config-yello-base/rules/typescript.js
+++ packages/eslint-config-yello-base/rules/typescript.js
@@ -1,6 +1,16 @@
 
 module.exports = {
 	rules: {
-		'@typescript-eslint/ban-types': 0
+		'@typescript-eslint/ban-types': 0,
+		// Needs to be disabled for non-TS files
+		'@typescript-eslint/explicit-function-return-type': 'off'
 	},
+	overrides: [
+		{
+			'files': ['*.ts', '*.tsx'],
+			'rules': {
+				'@typescript-eslint/explicit-function-return-type': ['error']
+			}
+		}
+	]
 };
