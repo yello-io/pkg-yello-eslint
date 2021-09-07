dc8d7853f2fd17f2f2758bc747cb6211ae6aa8d4
Marius Jørgensen <mariusjorg93@gmail.com>
2021-09-07 14:37:19 +0200

    Add jsx-a11y/anchor-is-valid

--- packages/eslint-config-yello-nextjs/rules/base.js
+++ packages/eslint-config-yello-nextjs/rules/base.js
@@ -3,5 +3,11 @@ module.exports = {
 	extends: [
 		'plugin:@next/next/recommended',
 		'@yello-io/eslint-config-yello-react'
-	]
+	],
+	rules: {
+		'jsx-a11y/anchor-is-valid': [ 'error', {
+			'components': [ 'Link' ],
+			'aspects': [ 'invalidHref', 'preferButton' ]
+		}]
+	}
 };
