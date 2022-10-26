48d0fda076dddd090aed3d103de993e2a08fdde2
Marius Jørgensen <mariusjorg93@gmail.com>
2022-10-26 23:29:42 +0200

    fix formatting

--- packages/eslint-config-yello-react/rules/unsorted.js
+++ packages/eslint-config-yello-react/rules/unsorted.js
@@ -6,13 +6,7 @@ module.exports = {
 		'react/jsx-one-expression-per-line': [0],
 		'react/prop-types': 0,
 		'react/no-unescaped-entities': 0,
-		'react/self-closing-comp': ['error', {
-			'component': true,
-			'html': true
-		}],
-		'jsx-a11y/anchor-is-valid': [ 'error', {
-			'components': [ 'Link' ],
-			'aspects': [ 'invalidHref', 'preferButton' ]
-		}]
+		'react/self-closing-comp': ['error', { 'component': true, 'html': true }],
+		'jsx-a11y/anchor-is-valid': [ 'error', { 'components': [ 'Link' ], 'aspects': [ 'invalidHref', 'preferButton' ] }]
 	}
 };
