93ea8df40abbcc8e36645e31edecc9df0745ebca
Leo Toneff <43667677+bragle@users.noreply.github.com>
2022-10-26 22:05:56 +0200

    changed linting

--- packages/eslint-config-yello-base/index.js
+++ packages/eslint-config-yello-base/index.js
@@ -1,22 +1,22 @@
-
 module.exports = {
-	extends: [
-		'./rules/base',
-		'./rules/unsorted',
-		'./rules/import',
-		'./rules/typescript'
-	].map(require.resolve),
+
+	extends: [ './rules/base', './rules/unsorted', './rules/import', './rules/typescript' ].map(require.resolve),
 	ignorePatterns: [ 'dist', 'build', '**/*.json' ],
 	env: {
+
 		browser: true,
 		es6: true,
 		node: true
+
 	},
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
+
 		ecmaVersion: 2018,
-		sourceType: 'module',
+		sourceType: 'module'
+
 	},
 	plugins: [ '@typescript-eslint' ],
 	rules: {}
+
 };
